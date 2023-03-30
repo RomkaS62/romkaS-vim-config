773c56047abcee162c25d3c208dad685a0452de1
Ramūnas Mažeikis <ramunas.mazeikis@teltonika.lt>
2023-03-30 10:41:20 +0300

    Diffview plugin is a must-have.
    Signed-off-by: Ramūnas Mažeikis <ramunas.mazeikis@teltonika.lt>

--- .vim/lua/plugins.lua
+++ .vim/lua/plugins.lua
@@ -2,4 +2,5 @@ local use = require('packer').use
 return require('packer').startup(function()
 	use 'wbthomason/packer.nvim'
 	use 'neovim/nvim-lspconfig'
+	use { 'sindrets/diffview.nvim', requires = 'nvim-lua/plenary.nvim' }
 end)
